d507ce6e32f050c47e5d1c7ccb9bbd4ec2f2786a
Cansiny0320 <1136843910@qq.com>
2021-04-01 21:57:07 +0800

    fix: ip 更换为本机内网ip

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 const fetch = require("node-fetch");
-const os = require('os');
+const os = require("os");
+const internalIp = require("internal-ip");
 const device = "0"; // 伪装设备，0 为电脑，1 为手机
 const account = "1658375"; // 帐号
 const passwd = "Tender520Family"; // 密码
 const type = "cmcc"; // cmcc 为移动，telecom 为电信
-const wifiList = ['CQUPT', 'CQUPT-2.4G', 'CQUPT-5G']; // 仅连接到以下网络时才运行
+const wifiList = ["CQUPT", "CQUPT-2.4G", "CQUPT-5G"]; // 仅连接到以下网络时才运行
 let stop = 5; // 无法登录重尝 5-1 次
 
 if (os.type() == "Windows_NT") {
   const wifi = require("node-wifi");
   wifi.init({
-    iface: null
+    iface: null,
   });
   wifi.getCurrentConnections((error, currentConnections) => {
     if (error) {
-      // console.log(error);
+      console.log(error);
     } else if (wifiList.includes(currentConnections[0].ssid)) {
       main();
     }
   });
 } else if (os.type() == "Linux") {
-  const { exec } = require('child_process');
-  exec('nmcli -t -f NAME connection show --active', (err, stdout) => {
+  const { exec } = require("child_process");
+  exec("nmcli -t -f NAME connection show --active", (err, stdout) => {
     if (err) {
       console.log(err);
       return;
     } else if (wifiList.includes(stdout.slice(0, -1))) {
       main();
     }
-  })
-} else if (os.type() == 'Darwin') {
+  });
+} else if (os.type() == "Darwin") {
   //mac 没有设备
 }
 
-function main() {
+async function main() {
   // 最多运行 4 次，一直连不上就是网络本身的问题
   if (!stop--) {
     console.log("无法登录，您可能欠费停机");
@@ -45,32 +46,25 @@ function main() {
    * msg: "认证成功" 登录成功
    * msg: "" 本机已登录
    * 其他就是 其他设备已登录
-  */
-  fetch(`http://192.168.200.2:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C${device}%2C${account}%40${type}&user_password=${passwd}&wlan_user_ip=${getIPAddress()}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=`)
+   */
+  fetch(
+    `http://192.168.200.2:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C${device}%2C${account}%40${type}&user_password=${passwd}&wlan_user_ip=${await getIPAddress()}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=`,
+  )
     .then(res => res.text())
     .then(res => JSON.parse(res.slice(7, -1)))
     .then(res => {
-      if (res.msg == '认证成功') {
+      if (res.msg == "认证成功") {
         console.log("登录成功");
         return;
-      } else if (res.msg == '') {
+      } else if (res.msg == "") {
         console.log("本机已登录");
         return;
       } else {
         main();
       }
-    })
+    });
 }
 
 function getIPAddress() {
-  var interfaces = os.networkInterfaces();
-  for (var devName in interfaces) {
-    var iface = interfaces[devName];
-    for (var i = 0; i < iface.length; i++) {
-      var alias = iface[i];
-      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
-        return alias.address;
-      }
-    }
-  }
+  return internalIp.v4();
 }
