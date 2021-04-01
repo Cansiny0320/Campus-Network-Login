396355e60764cfc8f239f0aae1047c6136648d02
Cansiny0320 <1136843910@qq.com>
2021-04-02 00:14:08 +0800

    feat: 支持windows自动连接WiFi

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Campus-Network-Login
+
 校园网一键登录，用于**重庆邮电大学**
+
 - 支持伪装设备类型，自由切换手机电脑
-- 识别 Wifi 类型，仅连接校园网时才执行脚本
+- 识别 WiFi 类型，windows 下可自动连接 WiFi，linux 下仅连接校园网时才执行脚本
 - 支持 Linux&Windows
 
 ### Usage
@@ -21,14 +23,10 @@ cd Campus-Network-Login & yarn
 node index.js
 ```
 
- Win 可以尝试搭配计划任务来实现开机自动登录，Linux 就不用多说了，自动执行的方法很多。Mac 的话由于我没有设备，不过你简单修改应该也可以。
+Win 可以尝试搭配计划任务来实现开机自动登录，Linux 就不用多说了，自动执行的方法很多。Mac 的话由于我没有设备，不过你简单修改应该也可以。
 
 ### More
 
 如果你的 iOS 设备同样需要，可以使用下方的快捷指令，也是我所写，可以结合自动化来实现连接校园网时执行快捷指令
 
 [一键登录校园网](https://www.icloud.com/shortcuts/f8c149eed3874a36a2f96283986b4e06)
-
-如果你想知道更多关于重邮校园网的信息，可以查看这篇文章
-
-[折腾校园网的那些事](https://www.notion.so/busiyi/12ad895be20f42e7bad68648c81adf0a)
--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-const fetch = require("node-fetch");
-const os = require("os");
-const internalIp = require("internal-ip");
-const device = "0"; // 伪装设备，0 为电脑，1 为手机
-const account = "1658375"; // 帐号
-const passwd = "Tender520Family"; // 密码
-const type = "cmcc"; // cmcc 为移动，telecom 为电信
-const wifiList = ["CQUPT", "CQUPT-2.4G", "CQUPT-5G"]; // 仅连接到以下网络时才运行
-let stop = 5; // 无法登录重尝 5-1 次
+const fetch = require("node-fetch")
+const os = require("os")
+const internalIp = require("internal-ip")
+const device = "0" // 伪装设备，0 为电脑，1 为手机
+const account = "" // 帐号
+const passwd = "" // 密码
+const type = "cmcc" // cmcc 为移动，telecom 为电信
+const wifiList = ["CQUPT-5G", "CQUPT", "CQUPT-2.4G"] // 校园网WiFi列表，按优先级排序
+let stop = 5 // 无法登录重尝 5-1 次
 
 if (os.type() == "Windows_NT") {
-  const wifi = require("node-wifi");
+  const wifi = require("node-wifi")
   wifi.init({
     iface: null,
-  });
-  wifi.getCurrentConnections((error, currentConnections) => {
+  })
+  wifi.scan((error, networks) => {
     if (error) {
-      console.log(error);
-    } else if (wifiList.includes(currentConnections[0].ssid)) {
-      main();
+      console.log(error)
+    } else {
+      // console.log(networks)
+      let ssid
+      const ssids = networks.map(item => {
+        if (wifiList.includes(item.ssid)) {
+          return item.ssid
+        }
+      })
+      wifiList.some(item => {
+        if (ssids.includes(item)) {
+          ssid = item
+          return true
+        }
+      })
+      if (ssid) {
+        wifi.connect({ ssid }, error => {
+          if (error) {
+            console.log(error)
+          }
+          main()
+        })
+      } else {
+        console.log("附近没有校园WiFi")
+      }
     }
-  });
+  })
 } else if (os.type() == "Linux") {
-  const { exec } = require("child_process");
+  const { exec } = require("child_process")
   exec("nmcli -t -f NAME connection show --active", (err, stdout) => {
     if (err) {
-      console.log(err);
-      return;
+      console.log(err)
+      return
     } else if (wifiList.includes(stdout.slice(0, -1))) {
-      main();
+      main()
     }
-  });
+  })
 } else if (os.type() == "Darwin") {
   //mac 没有设备
 }
@@ -37,8 +59,8 @@ if (os.type() == "Windows_NT") {
 async function main() {
   // 最多运行 4 次，一直连不上就是网络本身的问题
   if (!stop--) {
-    console.log("无法登录，您可能欠费停机");
-    return;
+    console.log("无法登录，您可能欠费停机或者校园网已关闭")
+    return
   }
 
   /*
@@ -47,6 +69,9 @@ async function main() {
    * msg: "" 本机已登录
    * 其他就是 其他设备已登录
    */
+  // console.log(
+  //   `http://192.168.200.2:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C${device}%2C${account}%40${type}&user_password=${passwd}&wlan_user_ip=${await getIPAddress()}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=`,
+  // )
   fetch(
     `http://192.168.200.2:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C${device}%2C${account}%40${type}&user_password=${passwd}&wlan_user_ip=${await getIPAddress()}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=`,
   )
@@ -54,17 +79,17 @@ async function main() {
     .then(res => JSON.parse(res.slice(7, -1)))
     .then(res => {
       if (res.msg == "认证成功") {
-        console.log("登录成功");
-        return;
+        console.log("登录成功")
+        return
       } else if (res.msg == "") {
-        console.log("本机已登录");
-        return;
+        console.log("本机已登录")
+        return
       } else {
-        main();
+        main()
       }
-    });
+    })
 }
 
 function getIPAddress() {
-  return internalIp.v4();
+  return internalIp.v4()
 }
