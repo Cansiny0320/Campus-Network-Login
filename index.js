8d3338b67c4b712d2fd414441f862cbbeeb97b25
Cansiny0320 <1136843910@qq.com>
2021-04-02 13:26:46 +0800

    feat: async/await

--- index.js
+++ index.js
@@ -57,39 +57,44 @@ if (os.type() == "Windows_NT") {
 }
 
 async function main() {
-  // 最多运行 4 次，一直连不上就是网络本身的问题
-  if (!stop--) {
-    console.log("无法登录，您可能欠费停机或者校园网已关闭")
-    return
-  }
+  try {
+    // 最多运行 4 次，一直连不上就是网络本身的问题
+    if (!stop--) {
+      console.log("无法登录，您可能欠费停机或者校园网已关闭")
+      return
+    }
+    console.log("login...")
+    /*
+     * 通过 msg 判断
+     * msg: "认证成功" 登录成功
+     * msg: "" 本机已登录
+     * 其他就是 其他设备已登录
+     */
+    const msg = await login()
 
-  /*
-   * 通过 msg 判断
-   * msg: "认证成功" 登录成功
-   * msg: "" 本机已登录
-   * 其他就是 其他设备已登录
-   */
-  // console.log(
-  //   `http://192.168.200.2:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C${device}%2C${account}%40${type}&user_password=${passwd}&wlan_user_ip=${await getIPAddress()}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=`,
-  // )
-  fetch(
-    `http://192.168.200.2:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C${device}%2C${account}%40${type}&user_password=${passwd}&wlan_user_ip=${await getIPAddress()}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=`,
-  )
-    .then(res => res.text())
-    .then(res => JSON.parse(res.slice(7, -1)))
-    .then(res => {
-      if (res.msg == "认证成功") {
-        console.log("登录成功")
-        return
-      } else if (res.msg == "") {
-        console.log("本机已登录")
-        return
-      } else {
-        main()
-      }
-    })
+    if (msg == "认证成功") {
+      console.log("登录成功")
+      return
+    } else if (msg == "") {
+      console.log("本机已登录")
+      return
+    } else {
+      main()
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 function getIPAddress() {
   return internalIp.v4()
+}
+
+async function login() {
+  const response = await fetch(
+    `http://192.168.200.2:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C${device}%2C${account}%40${type}&user_password=${passwd}&wlan_user_ip=${await getIPAddress()}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=`,
+  )
+  const info = await response.text()
+  const { msg } = JSON.parse(info.slice(7, -1))
+  return msg
 }
